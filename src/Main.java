b596dd005183a6144d6a247d564afa0e50bbe662
Anastasiya Leonova <nastena_andrej@mail.ru>
2022-04-11 21:13:07 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int amount = 20;
+        int bonusMiles = ticketPrice / amount;
+        System.out.println("Бонусные мили:" + bonusMiles);
+    }
+}
